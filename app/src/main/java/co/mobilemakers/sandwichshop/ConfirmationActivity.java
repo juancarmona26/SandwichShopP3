26099d0dffe14ede1333f56710fa2a108223b5ac
juancarmona26 <juan.carmona@globant.com>
2015-01-27 21:44:02 -0600

    Revert "Confirmation screen created"
    This reverts commit 31056f276f81ea053eb23ab83a74986736592ca8.

--- app/src/main/java/co/mobilemakers/sandwichshop/ConfirmationActivity.java
+++ app/src/main/java/co/mobilemakers/sandwichshop/ConfirmationActivity.java
@@ -1,79 +1,17 @@
 package co.mobilemakers.sandwichshop;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.List;
 
 
 public class ConfirmationActivity extends ActionBarActivity {
-
-    private String mBread;
-    private List<String> mSelectedCondiments;
-    private TextView mTextViewBreadSelected;
-    private TextView mTextViewCondimentsSelected;
-    private Button mButtonOk;
-    private Button mButtonCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
-
-        mBread = getIntent().getStringExtra("bread");
-        mSelectedCondiments = (List<String>) getIntent().getSerializableExtra("condiments");
-
-        prepareViewsToShowConfirmation();
-        setValuesToTextsViews();
-        setButtonConfirmationEvents();
-
-
-    }
-
-    private void prepareViewsToShowConfirmation(){
-        mTextViewBreadSelected = (TextView) findViewById(R.id.text_view_bread_selected);
-        mTextViewCondimentsSelected = (TextView) findViewById(R.id.text__view_condiments_selected);
-        mButtonOk = (Button) findViewById(R.id.button_ok);
-        mButtonCancel = (Button) findViewById(R.id.button_cancel);
-    }
-
-    private void setValuesToTextsViews() {
-        StringBuffer condimentsTextToShow = new StringBuffer();
-        mTextViewBreadSelected.setText(mBread);
-
-        if(mSelectedCondiments.size() != 0 ) {
-            for (int i = 0; i < mSelectedCondiments.size(); i++){
-                condimentsTextToShow.append("- ").append(mSelectedCondiments.get(i)).append("\n");
-            }
-            mTextViewCondimentsSelected.setText(condimentsTextToShow);
-        } else {
-            mTextViewCondimentsSelected.setText("No condiments selected");
-        }
-    }
-
-    private void setButtonConfirmationEvents() {
-        mButtonOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Your Order has been Placed", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        });
-
-        mButtonCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Your order has been canceled", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        });
-
     }
 
 
